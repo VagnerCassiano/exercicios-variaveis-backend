bce2e0f1342421d229a515b5b4eb6a9521c14e6b
VagnerCassiano <juniorcassiano.dev@gmail.com>
2023-07-10 18:44:35 -0300

    adicionando simbolo de percentual no código

--- 03/index.js
+++ 03/index.js
@@ -2,4 +2,4 @@ let valorDoTenis = 129.99
 let valorEmConta = 80
 let valorDesconto = ((valorDoTenis - valorEmConta) / valorDoTenis) * 100
 
-console.log(valorDesconto);
+console.log(valorDesconto + "%");
