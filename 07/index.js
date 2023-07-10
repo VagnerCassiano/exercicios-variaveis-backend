3c8c1d9bf28c201d6cbef0b95900b819d22b870c
VagnerCassiano <juniorcassiano.dev@gmail.com>
2023-07-10 19:58:48 -0300

    Resolucao exercício 07

---
+++ 07/index.js
@@ -0,0 +1,6 @@
+let populacaoInicial = 1000;
+let quantidadePessoasTransmitir = 4;
+let tempoPercorrido = 100;
+
+let P = (populacaoInicial * (quantidadePessoasTransmitir ** (tempoPercorrido/7)));
+console.log(P);
