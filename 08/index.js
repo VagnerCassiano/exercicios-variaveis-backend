721cce41382e7f810c2f8f4d385f91bd074dd7ee
VagnerCassiano <juniorcassiano.dev@gmail.com>
2023-07-10 20:27:18 -0300

    Resolução exercício 08

---
+++ 08/index.js
@@ -0,0 +1,7 @@
+let raio = 1;
+
+let comprimentoDoCirculo = 2 * Math.PI * raio;
+let areaDoCirculo = Math.PI * raio ** 2;
+
+console.log("O comprimento do círculo é: " + comprimentoDoCirculo.toFixed(2));
+console.log("A área do círculo é: " + areaDoCirculo.toFixed(2));
