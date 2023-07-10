8333036ab7c210190c0349ded4a410f6b28b8ee8
VagnerCassiano <juniorcassiano.dev@gmail.com>
2023-07-10 20:37:48 -0300

    Resolução exercício 09

---
+++ 09/index.js
@@ -0,0 +1,6 @@
+let numeroLados = 6;
+let somaDosAngulos = (numeroLados - 2) * 180;
+let valorCadaAngulo = somaDosAngulos / numeroLados;
+
+console.log("A soma dos ângulos internos do polígono é: " + somaDosAngulos + "graus");
+console.log("O valor de cada ângulo do polígono é: " + valorCadaAngulo + "graus");
